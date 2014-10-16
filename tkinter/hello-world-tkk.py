d802ebeab32511dbf0a2f5d180f37a6864e7dc84
Rob Muhlestein <rob@skilstak.com>
2014-10-16 15:37:08 -0400

    save

---
+++ tkinter/hello-world-tkk.py
@@ -0,0 +1,38 @@
+'''
+This is 'hello world' in modern tkinter/ttk style.
+
+Don't worry if that doesn't make any sense, just know it is the most
+modern, accessible, and available way to make cool graphics apps in
+Python.
+
+For those that are familiar with tkinter we use `import tkinter as tk`
+as a standard (as opposed to what some call the "standard" alternative
+`from tkinter import *`) because it is the most explicit and makes the
+most sense to both those that are new to tkinter programming as well
+as those that have been doing it for decades. This is the same reason
+for `import tkinter.ttk as ttk` instead of the `from` alternative. The
+explicit prefixes ensure -- in Pythonic style -- there is never a question
+about which type of widget you are using and that's important because
+using the ttk widgets requires a different syntax for styling.
+'''
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+def do_print():
+    print('Hello World')
+
+def do_exit():
+    print('Ok, goodbye.')
+    exit()
+
+# use a root controller with a child frame since the root 
+# controller itself is not themable but the child is
+root = tk.Tk()
+main = ttk.Frame(root).pack()
+
+root.wm_title('Hello GUI World')
+button1 = ttk.Button(main, text="Don't Press Me", command=do_print).pack()
+button2 = ttk.Button(main, text="Had Enough?", command=do_exit).pack()
+
+input()
--- tkinter/triangle.py
+++ tkinter/triangle.py
@@ -1,7 +1,8 @@
-from tkinter import Tk,Canvas
+import tkinter as tk
+import tkinter.tkk import ttk
 
-tk = Tk()
-canvas = Canvas(tk, width=400, height=400)
+root = tk.Tk()
+canvas = tk.Canvas(root, width=60, height=70)
 canvas.pack()
 triangle = canvas.create_polygon(10,10,10,60,50,35)
 canvas.move(triangle,5,0)
