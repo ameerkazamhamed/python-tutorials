bcf0a9e7df1ca4f00d7a0502063d8119af4df7a4
Rob Muhlestein <rob@skilstak.com>
2014-11-06 09:48:51 -0500

    hasattr

---
+++ classes/hasattr.py
@@ -0,0 +1,25 @@
+class MyClass():
+    my_class_attr = 'blah'
+    overriden = 'lovely'
+
+    def __init__(self):
+        self.overriden = 'mine now'
+        self.another = 'ok'
+
+if __name__ == '__main__':
+    instance = MyClass()
+    print(instance)
+    print(instance.__dict__)
+    print('has myclass_attr',hasattr(MyClass,'my_class_attr'))
+    print('has myclass_attr',MyClass.my_class_attr)
+    print('has myclass_attr',hasattr(instance,'my_class_attr'))
+    print('has myclass_attr',instance.my_class_attr)
+    print('has overriden',hasattr(MyClass,'overriden'))
+    print('has overriden',MyClass.overriden)
+    print('has overriden',hasattr(instance,'overriden'))
+    print('has overriden',instance.overriden)
+    print('has another',hasattr(MyClass,'another'))
+    print('has another',MyClass.another)
+    print('has another',hasattr(instance,'another'))
+    print('has another',instance.another)
+
