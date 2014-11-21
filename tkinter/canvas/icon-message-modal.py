11559ebe66ea1703646297d8b38074629b6bceb2
Rob Muhlestein <rob@skilstak.com>
2014-11-21 15:24:21 -0500

    icon-message-modal started

--- tkinter/canvas/icon-message-modal.py
+++ tkinter/canvas/icon-message-modal.py
@@ -10,10 +10,20 @@ def round_rect(w,x,y,dx,dy,radius,color='white',tag='round_rect'):
     w.create_rectangle(x+half,y,dx-half,dy,fill=color,outline=color,tag=tag)
     w.create_rectangle(x,y+half,dx,dy-half,fill=color,outline=color,tag=tag)
 
+
+
+def icon_message(w,x,y,dx,dy,message="",image=None,color='grey',tag='icon_message'):
+    radius = 20
+    half = int(radius / 2)
+    round_rect(w,100,150,300,250,20,'black')
+    round_rect(w,100+5,150+5,300-5,250-5,10,'white')
+    w.create_image(x+40-50,y,image=image)
+
 root = tk.Tk()
 canvas = tk.Canvas(root, width=400, height=400)
 canvas.pack()
-round_rect(canvas,20,20,380,380,30,'grey')
+image = tk.PhotoImage(file='image.gif')
+icon_message(canvas,150,150,250,200,"Good Job!",image)
 canvas.tag_bind('round_rect','<Button-3>',lambda e: print(e.__dict__))
 canvas.bind_all('<Button-1>',lambda e: exit())
 root.mainloop()
